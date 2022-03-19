08e69acad93cbf626fb607618ddbadfe1794458b
Krzysztof Adamkiewicz <kadamkiewicz835@gmail.com>
2022-03-19 17:22:00 +0100

    Add missing changes for previous commit.

--- process.py
+++ process.py
@@ -1,15 +1,36 @@
+from actions import *
+
+def find_intents(lines, detector):
+    intents = []
+    for line in lines:
+        intents.append(detector(line))
+    return intents
+
+def process_intent(intent, request_text, preferences):
+    actions = {
+        "go_hospital": go_hospital_visit,
+        "go_covid_test": go_covid_test,
+        "fun_music": fun_music,
+        "find_zaklin": find_zaklin,
+        "eat": eat,
+        "shop": shop,
+        "walk_city": walk_city,
+        "walk_park": walk_park,
+        "get_drunk": get_drunk,
+        "go_cinema": go_cinema}
+
+    return actions[intent](request_text, preferences)
 
 def process_request(intent_detector, preference_detector, requests, preferences):
-    requests = requests.splitlines()
-    preferences = preferences.splitlines()
+    requests_lines = requests.splitlines()
+    preferences_lines = preferences.splitlines()
 
-    intents = []
-    for request in requests:
-        intent = intent_detector(request)
-        intents.append("intent: " + intent)
+    intents = find_intents(requests_lines, intent_detector)
+    preferences = find_intents(preferences_lines, preference_detector)
 
-    for preference in preferences:
-        intent = preference_detector(preference)
-        intents.append("preference: " + intent)
+    responses = []
 
-    return {"responses": intents}
+    for request, intent in zip(requests_lines, intents):
+        responses.append(process_intent(intent, request, preferences))
+
+    return {"responses": responses}
