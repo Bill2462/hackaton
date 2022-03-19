5af95b47b4571cd81fd7075105b5ebb33e86eb8c
Krzysztof Adamkiewicz <kadamkiewicz835@gmail.com>
2022-03-19 17:20:39 +0100

    Add code for calling different functions depending on intent.

---
+++ actions.py
@@ -0,0 +1,30 @@
+
+def go_hospital_visit(request_text, preferences):
+    return "hospital"
+
+def go_covid_test(request_text, preferences):
+    return "covid"
+
+def fun_music(request_text, preferences):
+    return "fun_music"
+
+def find_zaklin(request_text, preferences):
+    return "zaklin"
+
+def eat(request_text, preferences):
+    return "eat"
+
+def shop(request_text, preferences):
+    return "shop"
+
+def walk_city(request_text, preferences):
+    return "city"
+
+def walk_park(request_text, preferences):
+    return "walk_park"
+
+def get_drunk(request_text, preferences):
+    return "drunk"
+
+def go_cinema(request_text, preferences):
+    return "cinema"
